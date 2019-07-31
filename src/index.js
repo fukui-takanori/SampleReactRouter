aa8bb9b169db7c7703ebb023d8f6c296cddd1a7a
fukui_takanori <itfukui0922@icloud.com>
2019-07-31 18:19:28 +0900

    tmp

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
 ReactDOM.render(
-  <BrowserRouter basename=`${process.env.PUBLIC_URL}/SampleReactRouter`>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
